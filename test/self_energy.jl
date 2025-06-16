259f72cb8c86758e946b7ece3832e915b08e8b36
Orjan Ameye <orjan.ameye@hotmail.com>
2025-06-16 11:40:26 +0200

    refactor: replace OrderedCollections with SmallCollections for improved performance and consistency

--- test/self_energy.jl
+++ test/self_energy.jl
@@ -1,7 +1,7 @@
 using KeldyshContraction, Test
 
 @testset "self_energy_type" begin
-    using KeldyshContraction: self_energy_type, OrderedCollections, In, Out, Keldysh
-    dict = OrderedCollections.LittleDict(:in => Keldysh, :out => Keldysh)
+    using KeldyshContraction: self_energy_type, SmallCollections, In, Out, Keldysh
+    dict = SmallCollections.SmallDict{2}(:in => Keldysh, :out => Keldysh)
     @test_throws "should be zero" self_energy_type(dict)
 end
