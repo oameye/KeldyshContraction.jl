721486230da23ee4bab4da4798a7adfa5b329626
Orjan Ameye <orjan.ameye@hotmail.com>
2025-06-27 09:55:47 +0200

    test: add loss2boson second order reguralisation test

--- test/regularise.jl
+++ test/regularise.jl
@@ -13,7 +13,7 @@ using KeldyshContraction: Regularisation.Minus as Minus
     @test regular((c, q'))
 end
 
-@testset "regularise vs no regularise" begin
+@testset "regularise vs no regularise in elastic setting" begin
     using KeldyshContraction
     using KeldyshContraction: set_reg_to_zero
 
@@ -37,4 +37,35 @@ end
     @test isequal(GF_reg2.keldysh, GF_no_reg2.keldysh)
     @test isequal(GF_reg2.retarded, GF_no_reg2.retarded)
     @test isequal(GF_reg2.advanced, GF_no_reg2.advanced)
+end
+
+@testset "regularise vs no regularise in inelastic setting" begin
+    using KeldyshContraction
+
+    loss2boson_unregular =
+        im * (
+            0.5 * c' * q' * (c^2 + q^2) - 0.5 * c * q * ((c')^2 + (q')^2) +
+            c' * q' * (c * q + c * q)
+        )
+
+    loss2boson =
+        im * (
+            0.5 * c' * q' * (c(Minus) * c(Minus) + q(Minus) * q(Minus)) -
+            0.5 * c(Plus) * q(Plus) * (c' * c' + q' * q') +
+            c' * q' * (c(Plus) * q(Plus) + c(Minus) * q(Minus))
+        )
+
+    L_reg = InteractionLagrangian(loss2boson)
+    L_no_reg = InteractionLagrangian(loss2boson_unregular)
+
+    GF_reg2 = DressedPropagator(L_reg, 2; simplify=true, _set_reg_to_zero=true)
+    GF_no_reg2 = DressedPropagator(L_no_reg, 2; simplify=true, _set_reg_to_zero=true)
+
+    topology_reg = topologies(GF_reg2.keldysh)
+    topology_no_reg = topologies(GF_no_reg2.keldysh)
+
+    # The [3] multiplicity doesn't contain equal-time propagators, so the reguralisation shoudn't have any affect.
+    @test isequal(Set(topology_reg[[3]]), Set(topology_no_reg[[3]]))
+    @test !isequal(Set(topology_reg[[2]]), Set(topology_no_reg[[2]]))
+    @test !isequal(Set(topology_reg[[1]]), Set(topology_no_reg[[1]]))
 end
