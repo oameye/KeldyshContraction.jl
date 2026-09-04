5d49a26429c29b8610bb78ffd139b072d27351c6
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-05 00:31:22 +0200

    test: migrate second-order tests to bar fields

--- test/second_order.jl
+++ test/second_order.jl
@@ -4,14 +4,13 @@ using KeldyshContraction: Regularisation.Plus as Plus
 using KeldyshContraction: Regularisation.Minus as Minus
 using KeldyshContraction: Bulk, position
 
-@qfields ϕᶜ::Destroy(Classical) ϕᴾ::Destroy(Quantum)
+@qfields ϕᶜ::Boson(Classical) ϕᴾ::Boson(Quantum)
 
-L_int =
-    im*(
-        0.5 * ϕᶜ' * ϕᴾ' * (ϕᶜ(Minus) * ϕᶜ(Minus) + ϕᴾ(Minus) * ϕᴾ(Minus)) -
-        0.5 * ϕᶜ(Plus) * ϕᴾ(Plus) * (ϕᶜ' * ϕᶜ' + ϕᴾ' * ϕᴾ') +
-        ϕᶜ' * ϕᴾ' * (ϕᶜ(Plus) * ϕᴾ(Plus) + ϕᶜ(Minus) * ϕᴾ(Minus))
-    )
+L_int = im * (
+    0.5 * bar(ϕᶜ) * bar(ϕᴾ) * (ϕᶜ(Minus) * ϕᶜ(Minus) + ϕᴾ(Minus) * ϕᴾ(Minus)) -
+    0.5 * ϕᶜ(Plus) * ϕᴾ(Plus) * (bar(ϕᶜ) * bar(ϕᶜ) + bar(ϕᴾ) * bar(ϕᴾ)) +
+    bar(ϕᶜ) * bar(ϕᴾ) * (ϕᶜ(Plus) * ϕᴾ(Plus) + ϕᶜ(Minus) * ϕᴾ(Minus))
+)
 L = InteractionLagrangian(L_int)
 
 @testset "set_position" begin
@@ -19,74 +18,80 @@ L = InteractionLagrangian(L_int)
     @test position(L(2)) == Bulk(2)
 end
 
-@testset "ladder sorted" begin
+@testset "orientation sorted" begin
     L1 = L
     L2 = L(2)
     expr = L1.lagrangian * L2.lagrangian
     for arg in expr.arguments
         sorted = sort(arg.args_nc; by=KeldyshContraction.ladder)
-        @test isequal(arg.args_nc, sorted)
+        @test all(KeldyshContraction.ladder.(arg.args_nc) .== KeldyshContraction.ladder.(sorted))
     end
 end
 
 @testset "zero loop filter" begin
     using KeldyshContraction: has_zero_loop, Contraction
-    # Gᴿ(1,2) Gᴿ(2,1) = 0
-    vs = Contraction[(ϕᶜ(Bulk(1)), ϕᴾ'(Bulk(2))), (ϕᶜ(Bulk(2)), ϕᴾ'(Bulk(1)))]
-    @test has_zero_loop(vs)
-
-    # Gᴬ(1,2) Gᴬ(2,1) = 0
-    vs = Contraction[(ϕᴾ(Bulk(1)), ϕᶜ'(Bulk(2))), (ϕᴾ(Bulk(2)), ϕᶜ'(Bulk(1)))]
-    @test has_zero_loop(vs)
-
-    # Gᴿ(1,2) Gᴬ(1,2) = 0
-    vs = Contraction[(ϕᶜ(Bulk(1)), ϕᴾ'(Bulk(2))), (ϕᴾ(Bulk(1)), ϕᶜ'(Bulk(2)))]
-    @test has_zero_loop(vs)
-
-    vs = Contraction[(ϕᶜ(Bulk(1)), ϕᴾ'(Bulk(2))), (ϕᶜ(Bulk(1)), ϕᴾ'(Bulk(2)))]
-    @test !has_zero_loop(vs)
-
-    # A causal cycle can contain more than two propagators.
     vs = Contraction[
-        (ϕᶜ(Bulk(1)), ϕᴾ'(Bulk(2))),
-        (ϕᶜ(Bulk(2)), ϕᴾ'(Bulk(3))),
-        (ϕᶜ(Bulk(3)), ϕᴾ'(Bulk(1))),
+        (ϕᶜ(Bulk(1)), bar(ϕᴾ)(Bulk(2))),
+        (ϕᶜ(Bulk(2)), bar(ϕᴾ)(Bulk(1))),
     ]
     @test has_zero_loop(vs)
 
     vs = Contraction[
-        (ϕᴾ(Bulk(1)), ϕᶜ'(Bulk(2))),
-        (ϕᴾ(Bulk(2)), ϕᶜ'(Bulk(3))),
-        (ϕᴾ(Bulk(3)), ϕᶜ'(Bulk(1))),
-    ]
-    @test has_zero_loop(vs)
-
-    # Mixed retarded/advanced constraints can also be inconsistent.
-    vs = Contraction[
-        (ϕᴾ(Bulk(1)), ϕᶜ'(Bulk(2))),
-        (ϕᶜ(Bulk(1)), ϕᴾ'(Bulk(3))),
-        (ϕᶜ(Bulk(3)), ϕᴾ'(Bulk(2))),
+        (ϕᴾ(Bulk(1)), bar(ϕᶜ)(Bulk(2))),
+        (ϕᴾ(Bulk(2)), bar(ϕᶜ)(Bulk(1))),
     ]
     @test has_zero_loop(vs)
 
     vs = Contraction[
-        (ϕᴾ(Bulk(1)), ϕᶜ'(Bulk(2))),
-        (ϕᶜ(Bulk(1)), ϕᴾ'(Bulk(3))),
-        (ϕᶜ(Bulk(2)), ϕᴾ'(Bulk(3))),
+        (ϕᶜ(Bulk(1)), bar(ϕᴾ)(Bulk(2))),
+        (ϕᴾ(Bulk(1)), bar(ϕᶜ)(Bulk(2))),
+    ]
+    @test has_zero_loop(vs)
+
+    vs = Contraction[
+        (ϕᶜ(Bulk(1)), bar(ϕᴾ)(Bulk(2))),
+        (ϕᶜ(Bulk(1)), bar(ϕᴾ)(Bulk(2))),
     ]
     @test !has_zero_loop(vs)
 
-    # Keldysh propagators do not impose causal constraints.
     vs = Contraction[
-        (ϕᶜ(Bulk(1)), ϕᶜ'(Bulk(2))),
-        (ϕᶜ(Bulk(2)), ϕᶜ'(Bulk(3))),
-        (ϕᶜ(Bulk(3)), ϕᶜ'(Bulk(1))),
+        (ϕᶜ(Bulk(1)), bar(ϕᴾ)(Bulk(2))),
+        (ϕᶜ(Bulk(2)), bar(ϕᴾ)(Bulk(3))),
+        (ϕᶜ(Bulk(3)), bar(ϕᴾ)(Bulk(1))),
+    ]
+    @test has_zero_loop(vs)
+
+    vs = Contraction[
+        (ϕᴾ(Bulk(1)), bar(ϕᶜ)(Bulk(2))),
+        (ϕᴾ(Bulk(2)), bar(ϕᶜ)(Bulk(3))),
+        (ϕᴾ(Bulk(3)), bar(ϕᶜ)(Bulk(1))),
+    ]
+    @test has_zero_loop(vs)
+
+    vs = Contraction[
+        (ϕᴾ(Bulk(1)), bar(ϕᶜ)(Bulk(2))),
+        (ϕᶜ(Bulk(1)), bar(ϕᴾ)(Bulk(3))),
+        (ϕᶜ(Bulk(3)), bar(ϕᴾ)(Bulk(2))),
+    ]
+    @test has_zero_loop(vs)
+
+    vs = Contraction[
+        (ϕᴾ(Bulk(1)), bar(ϕᶜ)(Bulk(2))),
+        (ϕᶜ(Bulk(1)), bar(ϕᴾ)(Bulk(3))),
+        (ϕᶜ(Bulk(2)), bar(ϕᴾ)(Bulk(3))),
     ]
     @test !has_zero_loop(vs)
 
-    retarded(i, j) = (ϕᶜ(Bulk(i)), ϕᴾ'(Bulk(j)))
-    advanced(i, j) = (ϕᴾ(Bulk(i)), ϕᶜ'(Bulk(j)))
-    keldysh(i, j) = (ϕᶜ(Bulk(i)), ϕᶜ'(Bulk(j)))
+    vs = Contraction[
+        (ϕᶜ(Bulk(1)), bar(ϕᶜ)(Bulk(2))),
+        (ϕᶜ(Bulk(2)), bar(ϕᶜ)(Bulk(3))),
+        (ϕᶜ(Bulk(3)), bar(ϕᶜ)(Bulk(1))),
+    ]
+    @test !has_zero_loop(vs)
+
+    retarded(i, j) = (ϕᶜ(Bulk(i)), bar(ϕᴾ)(Bulk(j)))
+    advanced(i, j) = (ϕᴾ(Bulk(i)), bar(ϕᶜ)(Bulk(j)))
+    keldysh(i, j) = (ϕᶜ(Bulk(i)), bar(ϕᶜ)(Bulk(j)))
 
     @testset "longer homogeneous cycles" begin
         @test has_zero_loop(
@@ -114,7 +119,6 @@ end
         ]
         @test has_zero_loop(vs)
 
-        # The cycle is independent of the input order and of the extra edges.
         vs = [vs[4], vs[2], vs[6], vs[1], vs[3], vs[5]]
         @test has_zero_loop(vs)
     end
