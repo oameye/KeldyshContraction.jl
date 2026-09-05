5c1fd8d53d5248dfc10ad1225e40d278f0b0e6a2
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-05 19:26:43 +0200

    test: migrate Wigner tests to field-family API

--- test/wigner.jl
+++ test/wigner.jl
@@ -2,7 +2,8 @@ using KeldyshContraction, Test
 using KeldyshContraction:
     construct_linear_system, solve_linear_system, construct_momenta, momenta
 
-@qfields c::Boson(Classical) q::Boson(Quantum)
+@qfields ϕ::Boson
+c, q = ϕ[Classical], ϕ[Quantum]
 elasctic2boson = -(
     0.5 * (c^2 + q^2) * bar(c) * bar(q) + 0.5 * c * q * (bar(c)^2 + bar(q)^2)
 )
