3ad63b11fbb35f1ee4a4e6f3e18a556b1ef4bb9e
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-05 08:26:02 +0200

    benchmarks: migrate scattering fields to bar API

--- benchmarks/two_body_scattering.jl
+++ benchmarks/two_body_scattering.jl
@@ -1,6 +1,8 @@
 function benchmark_two_body_scattering!(SUITE)
-    @qfields c::Destroy(Classical) q::Destroy(Quantum)
-    elasctic2boson = -(0.5 * (c^2 + q^2) * c' * q' + 0.5 * c * q * ((c')^2 + (q')^2))
+    @qfields c::Boson(Classical) q::Boson(Quantum)
+    elasctic2boson = -(
+        0.5 * (c^2 + q^2) * bar(c) * bar(q) + 0.5 * c * q * (bar(c)^2 + bar(q)^2)
+    )
     L_int = InteractionLagrangian(elasctic2boson)
 
     GF = DressedPropagator(L_int, Val(1), Val(3))
