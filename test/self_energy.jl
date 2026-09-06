57f1350b235460b1f16d57db9ef7bc7370c4f61e
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-06 08:25:55 +0200

    test: dispatch self-energy layout on statistics

--- test/self_energy.jl
+++ test/self_energy.jl
@@ -1,8 +1,8 @@
 using KeldyshContraction, Test
 
 @testset "self_energy_type" begin
-    using KeldyshContraction: self_energy_type, SmallCollections, In, Out
+    using KeldyshContraction: self_energy_type, SmallCollections
     using KeldyshContraction: PropagatorType.Keldysh as Keldysh
     dict = SmallCollections.SmallDict{2}(:in => Keldysh, :out => Keldysh)
-    @test_throws "should be zero" self_energy_type(dict)
+    @test_throws "should be zero" self_energy_type(Boson, dict)
 end
