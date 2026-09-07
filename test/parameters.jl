ac16daef94ea9ef783ea87c09bc60d8d528e22df
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-07 14:29:39 +0200

    test: cover public parameter API

--- test/parameters.jl
+++ test/parameters.jl
@@ -16,4 +16,17 @@ import KeldyshContraction as KC
     @test eltype(gₘ.powers) === KC.ParameterPower
     @test isconcretetype(typeof(gₘ))
     @test isconcretetype(eltype(gₘ.powers))
+
+    @test @inferred(parameter_monomial(gₘ)) === gₘ
+    @test @inferred(parameter_monomial(:g)) == gₘ
+    @test @inferred(parameter_monomial(1)) == one(ParameterMonomial)
+    @test isone(one(ParameterMonomial))
+    @test !isone(gₘ)
+    @test_throws ArgumentError parameter_monomial(2)
+    @test_throws DomainError gₘ^(-1)
+    @test_throws ArgumentError parameter_monomial(g + λ)
+
+    @test sprint(show, one(ParameterMonomial)) == "1"
+    @test sprint(show, gₘ) == "g"
+    @test sprint(show, gₘ^2 * λₘ) == "g^2*λ"
 end
