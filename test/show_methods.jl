691865934b50f468e785cc2dc5f846b731f6cec2
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-07 11:26:09 +0200

    test: infer package-owned diagram rendering

--- test/show_methods.jl
+++ test/show_methods.jl
@@ -97,7 +97,7 @@ end
     @test repr(MIME"text/plain"(), DP) ==
         "Dressed Propagator:\nkeldysh:  Gᴷ(y₁,y₁)\nretarded: Gᴷ(y₁,y₁)\nadvanced: Gᴷ(y₁,y₁)"
 
-    diagram = first(ds)
+    diagram = first(keys(ds.diagrams))
     io = IOBuffer()
     @test @inferred(show(io, diagram)) === nothing
     io = IOBuffer()
