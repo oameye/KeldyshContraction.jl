2bbfc89e5da8665d87c1b776dc6f8596e5fafa9a
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-14 17:15:50 +0200

    test: match Documenter LaTeX display contract

--- test/collision_kernel_api.jl
+++ test/collision_kernel_api.jl
@@ -70,9 +70,9 @@ end
 
     for (value, expected) in displays
         rendered = sprint(show, MIME"text/latex"(), value)
-        @test startswith(rendered, "\\[")
+        @test startswith(rendered, "$$")
         @test occursin(expected, rendered)
-        @test endswith(rendered, "\\]")
+        @test endswith(rendered, "$$")
     end
 
     @test occursin("Kadanoff-Baym", sprint(show, MIME"text/plain"(), stages.I))
