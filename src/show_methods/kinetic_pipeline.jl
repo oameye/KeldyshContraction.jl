36940124fa711683846d283bb9e4637743190b56
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-14 18:43:50 +0200

    fix: accept Documenter display writer in do-block order

--- src/show_methods/kinetic_pipeline.jl
+++ src/show_methods/kinetic_pipeline.jl
@@ -47,7 +47,7 @@ function _write_latex_stage_metadata(io::IO, x)
     return nothing
 end
 
-function _latex_display(io::IO, writer)
+function _latex_display(writer, io::IO)
     write(io, "\\[\n")
     writer()
     write(io, "\n\\]")
