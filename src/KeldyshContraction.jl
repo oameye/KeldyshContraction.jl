1e611506f1c5750ed5ecc44ea23021767777f670
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-14 19:00:22 +0200

    test: validate end-to-end bosonic and fermionic loss kinetics (#312)

--- src/KeldyshContraction.jl
+++ src/KeldyshContraction.jl
@@ -70,6 +70,7 @@ const ComplexRationals = Complex{Rational{Int64}}
     include("trotter_frequency_collision.jl")
     include("collision_frequency_assembly.jl")
     include("collision_momentum_quotient.jl")
+    include("collision_momentum_projective.jl")
 
     # Legacy private routing used only by the pre-Fourier collision reducer.
     include("propagator_algebra/legacy_momentum_routing.jl")
