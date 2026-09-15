a8947f8a118543088d6dcd1d874d08a411fdca52
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-15 12:28:39 +0200

    test: certify compact regulator notation

--- test/collision_kernel_api.jl
+++ test/collision_kernel_api.jl
@@ -116,11 +116,16 @@ end
         @test occursin("\\gamma", rendered)
         @test endswith(rendered, "\$\$")
         @test !occursin("\\[", rendered)
-        @test !occursin(r"G\^[KRA]_\{[^}]+\}\^\{\[\\Delta t=", rendered)
+        @test !occursin("\\Delta t", rendered)
     end
 
     routed = sprint(show, MIME"text/latex"(), stages.GF)
-    @test occursin(r"G\^\{[KRA],\[\\Delta t=[+-]1\\,0\^\+\]\}_\{", routed)
+    @test occursin(r"G_\{[^}]+,[+-]\}\^\{[KRA]\}", routed)
+    @test !occursin(r"G\^\{[KRA],", routed)
+
+    sd = sprint(show, MIME"text/latex"(), stages.SD)
+    @test occursin(r"[AD]_\{[^}]+,[+-]\}", sd)
+    @test !occursin("0^+", sd)
 
     @test occursin("Kadanoff-Baym", sprint(show, MIME"text/plain"(), stages.I))
     @test occursin("causal blockers: 0", sprint(show, MIME"text/plain"(), stages.R))
