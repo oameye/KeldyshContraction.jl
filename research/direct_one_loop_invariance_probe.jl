effc8b967c04142fc67b61d9caac705b6a79b6ac
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-17 16:45:03 +0200

    research: build reflected occupation polynomial canonically

--- research/direct_one_loop_invariance_probe.jl
+++ research/direct_one_loop_invariance_probe.jl
@@ -8,14 +8,16 @@ function reflected_one_loop_expression(
         reflection = one_loop_reflection(sector)
         transformed_sector, support_factor = KC._transform_kernel_sector(sector, reflection)
         reflected_sector = reduced_sector(transformed_sector)
-        reflected_polynomial = KC.OccupationPolynomial{C,S}()
+        reflected_terms = Pair{KC.OccupationMonomial{S},C}[]
+        sizehint!(reflected_terms, length(polynomial))
         for (monomial, coefficient) in polynomial
             reflected_monomial = KC.transform_loop_momenta(monomial, reflection)
-            reflected_polynomial[reflected_monomial] =
-                get(reflected_polynomial, reflected_monomial, zero(C)) +
-                coefficient * convert(C, support_factor)
+            push!(
+                reflected_terms,
+                reflected_monomial => coefficient * convert(C, support_factor),
+            )
         end
-        out[reflected_sector] = reflected_polynomial
+        out[reflected_sector] = KC.OccupationPolynomial{C,S}(reflected_terms)
     end
     return KC.OccupationReducedExpression{C,S,O,G,Ctx}(
         out,
