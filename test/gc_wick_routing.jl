0441e35647c4e7e1ffe0666926c7a13538a73738
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-19 22:41:37 +0200

    test: isolate weighted Wick routing oracle

--- test/gc_wick_routing.jl
+++ test/gc_wick_routing.jl
@@ -1,6 +1,17 @@
 using KeldyshContraction, Test
 import KeldyshContraction as KC
 
+function routing_pairing_weights(pairings)
+    weights = Dict{Any,Int}()
+    for (pairing, topology, multiplicity) in pairings
+        key = (pairing.contractions, topology)
+        weight = Int(pairing.sign) * multiplicity
+        weights[key] = get(weights, key, 0) + weight
+    end
+    filter!(pair -> !iszero(last(pair)), weights)
+    return weights
+end
+
 @testset "GraphCombinations Wick production crossover" begin
     @test KC._use_gc_wick(Boson, Val(5))
     @test KC._use_gc_wick(Boson, Val(7))
@@ -13,14 +24,16 @@ import KeldyshContraction as KC
         0.5 * (c^2 + q^2) * bar(c) * bar(q) + 0.5 * c * q * (bar(c)^2 + bar(q)^2)
     )
     L_b = InteractionLagrangian(boson_vertex)
-    boson_term = first(KC.terms(c(KC.Out()) * bar(c)(KC.In()) * L_b(1).lagrangian * L_b(2).lagrangian))
+    boson_term = first(
+        KC.terms(c(KC.Out()) * bar(c)(KC.In()) * L_b(1).lagrangian * L_b(2).lagrangian)
+    )
     direct_b = KC._wick_contraction(
         boson_term.args_nc, Val(5), Val(1); regularise=true, simplify=true
     )
     routed_b = KC._production_wick_pairings(
         boson_term.args_nc, Val(5), Val(1); regularise=true, simplify=true
     )
-    @test signed_pairing_weights(routed_b) == signed_pairing_weights(direct_b)
+    @test routing_pairing_weights(routed_b) == routing_pairing_weights(direct_b)
 
     @qfields routing_ψ::Fermion
     ψ₁, ψ₂ = routing_ψ[One], routing_ψ[Two]
@@ -34,7 +47,7 @@ import KeldyshContraction as KC
     routed_f2 = KC._production_wick_pairings(
         fermion_term2.args_nc, Val(5), Val(1); regularise=false, simplify=false
     )
-    @test signed_pairing_weights(routed_f2) == signed_pairing_weights(direct_f2)
+    @test routing_pairing_weights(routed_f2) == routing_pairing_weights(direct_f2)
 
     fermion_term3 = first(
         KC.terms(first(products) * L_f(1).lagrangian * L_f(2).lagrangian * L_f(3).lagrangian)
@@ -45,5 +58,5 @@ import KeldyshContraction as KC
     routed_f3 = KC._production_wick_pairings(
         fermion_term3.args_nc, Val(7), Val(3); regularise=false, simplify=false
     )
-    @test signed_pairing_weights(routed_f3) == signed_pairing_weights(direct_f3)
+    @test routing_pairing_weights(routed_f3) == routing_pairing_weights(direct_f3)
 end
