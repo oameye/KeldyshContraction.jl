afbc30e72883749739fde5f6e3935abf497e9dde
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-20 10:50:06 +0200

    style: format direct self-energy test

--- test/direct_self_energy.jl
+++ test/direct_self_energy.jl
@@ -4,10 +4,7 @@ import KeldyshContraction as KC
 @testset "Direct self-energy generation" begin
     @qfields direct_Σ_ϕ::Boson
     c, q = direct_Σ_ϕ[Classical], direct_Σ_ϕ[Quantum]
-    elastic = -(
-        0.5 * (c^2 + q^2) * bar(c) * bar(q) +
-        0.5 * c * q * (bar(c)^2 + bar(q)^2)
-    )
+    elastic = -(0.5 * (c^2 + q^2) * bar(c) * bar(q) + 0.5 * c * q * (bar(c)^2 + bar(q)^2))
     L = InteractionLagrangian(elastic)
 
     for (order, edges) in ((Val(2), Val(5)), (Val(3), Val(7)))
